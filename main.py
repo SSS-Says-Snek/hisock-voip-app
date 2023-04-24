14e6f29fb5e2e8187af7116548dd803a4c938e75
Brandon <bmcomis2018@gmail.com>
2023-04-23 21:38:02 -0500

    Allow typing of messages

--- main.py
+++ main.py
@@ -20,19 +20,36 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.messageToSend.returnPressed.connect(self.send_message)
         self.sendButton.clicked.connect(self.send_message)
 
+        scrollarea_vbar = self.message_scrollarea.verticalScrollBar()
+
+        # Maybe this will be changed later so that people can view older messages while other people're talking
+        scrollarea_vbar.rangeChanged.connect(lambda: scrollarea_vbar.setValue(scrollarea_vbar.maximum()))
+
+        self.username = "SSS_Says_Snek"
+
         haha_messages = [
-            Message("SSS_Says_Snek", "Wowwwww \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn\n\n\namogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus !!"),
-            Message("SSS_Says_Snek", "cool"),
-            Message("SSS_Says_Snek", "cool"),
-            Message("SSS_Says_Snek", "cool"),
-            Message("SSS_Says_Snek", "cool")
+            "Wowwwww \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nn\n\n\namogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus Wowwwww amogus !!",
+            "cool",
+            "cool",
+            "cool"
         ]
 
         for message in haha_messages:
-            self.messages.addWidget(message)
+            self.messages.addWidget(Message(self.username, message))
 
     
     def send_message(self):
+        text = self.messageToSend.text()
+        self.messages.addWidget(
+            Message(self.username, text)
+        )
+        
+        # x = self.message_scrollarea.verticalScrollBar().maximum()
+        # self.message_scrollarea.verticalScrollBar().setValue(
+        #     x
+        # )
+        # scrollarea_vbar.setValue(scrollarea_vbar.maximum())
+
         self.messageToSend.clear()
 
 
