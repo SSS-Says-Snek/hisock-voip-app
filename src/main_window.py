69f2965e954a3330dbaf6f1a2d23fc86a98df5d3
Brandon <bmcomis2018@gmail.com>
2023-06-27 02:14:32 -0500

    Hopefully

--- src/main_window.py
+++ src/main_window.py
@@ -180,7 +180,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.video_cap_worker.moveToThread(self.video_cap_thread)
 
         self.video_cap_thread.started.connect(self.video_cap_worker.run)
-        self.video_cap_thread.finished.connect(self.actual_close)
+        self.video_cap_thread.finished.connect(self.voip_close)
         self.video_cap_worker.frame.connect(self.on_video_frame)
 
         self.video_cap_thread.start()
@@ -210,7 +210,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.frame_bar.mouseMoveEvent = self.move_window
 
         self.minimize_button.clicked.connect(self.showMinimized)
-        self.x_button.clicked.connect(self.video_cap_worker.finish)
+        self.x_button.clicked.connect(self.normal_close)
 
         self.everyone_message_to_send.returnPressed.connect(self.send_everyone_message)
         self.everyone_send_button.clicked.connect(self.send_everyone_message)
@@ -316,15 +316,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             Message("[Server]", (datetime.now() if time is None else time).strftime(self.TIME_FMT), message)
         )
 
-    def actual_close(self):
+    def normal_close(self):
         if self.notif is not None:
             self.notif.finish()
         
         self.video_cap_worker.cleanup()
 
         if self.video_cap_worker.calling_someone:
-            self.client.send("end_call", self.current_call_username())
-            self.client.recv("ended_call")
+            self.video_cap_worker.finish()
+        else:
+            self.client.close()
+            self.close()
+    
+    def voip_close(self):
+        self.client.send("end_call", self.current_call_username())
+        self.client.recv("ended_call")
 
         self.client.close()
         self.close()
